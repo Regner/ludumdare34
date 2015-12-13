1c51e7f178b65cf7177827089834bab6cdb42bf9
Regner Blok-Andersen <shadowdf@gmail.com>
2015-12-13 10:13:00 +0000

    Finished basic collision and added basic respawning

--- ld34.py
+++ ld34.py
@@ -3,8 +3,8 @@
 import sys
 import pygame
 import pyscroll
-import logging
 
+from collections import defaultdict
 from pytmx.util_pygame import load_pygame
 
 BLACK = (0, 0, 0)
@@ -30,7 +30,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('assets/player_start.png').convert()
         self.rect = self.image.get_rect()
         self._position = [200, 400]
-        self.velocity = [300, 300]
+        self.velocity = [400, 800]
+        self.dead = False
 
     @property
     def position(self):
@@ -40,7 +41,7 @@ class Player(pygame.sprite.Sprite):
     def position(self, value):
         self._position = list(value)
 
-    def update(self, dt, colliders):
+    def update(self, dt, obstacles):
         # Handle X position update
         self._position[0] += self.velocity[0] * dt
         self.rect.topleft = self._position
@@ -48,22 +49,30 @@ class Player(pygame.sprite.Sprite):
         # If we collide at this point with anything it means
         # we hit the side of the player. Unless it is the end
         # of the level that is a death.
-        collision_index = self.rect.collidelistall(colliders)
+        combined = obstacles['walls'] + obstacles['traps']
+        collision_index = self.rect.collidelistall(combined)
         if len(collision_index) > 0:
-            print 'You dun fucked up yo! Side impact!', colliders[collision_index]
+            self.dead = True
+            print 'DEATH!'
+            return
 
         # Handle Y position update
+        original_y = self._position[1]
         self._position[1] += self.velocity[1] * dt
         self.rect.topleft = self._position
 
         # If we collide at this point with anything it means
         # we hit the top or bottom of the player. This could
         # be good or bad. Need to dig deeper!
-        collision_index = self.rect.collidelistall(colliders)
+        collision_index = self.rect.collidelistall(obstacles['walls'])
         if len(collision_index) > 0:
-            print 'Possible fuck up... collision on top or bot...', colliders[collision_index]
+            self._position[1] = original_y
+            self.rect.topleft = self._position
 
-
+        collision_index = self.rect.collidelistall(obstacles['traps'])
+        if len(collision_index) > 0:
+            self.dead = True
+            print 'DEATH!'
 
 
 class LD34Game(object):
@@ -80,32 +89,50 @@ class LD34Game(object):
         # Gameplay Setup
         self.running = False
         self.player = Player()
+        self.player_spawn = [200, 400]
 
         # Basic level setup
-        self.walls = []
+        self.obstacles = defaultdict(list)
         self.map_group = None
 
         self.load_level()
 
     def load_level(self):
         tmx_data = load_pygame('untitled.tmx')
-
-        for obj in tmx_data.objects:
-            self.walls.append(pygame.Rect(
-                obj.x, obj.y,
-                obj.width, obj.height
-            ))
-
         map_data = pyscroll.TiledMapData(tmx_data)
         map_layer = pyscroll.BufferedRenderer(map_data, self.screen_size)
 
+        for obj in tmx_data.objects:
+            if 'gameType' in obj.properties:
+                if obj.properties['gameType'] == 'wall':
+                    self.obstacles['walls'].append(pygame.Rect(
+                        obj.x, obj.y,
+                        obj.width, obj.height
+                    ))
+
+                elif obj.properties['gameType'] == 'trap':
+                    self.obstacles['traps'].append(pygame.Rect(
+                        obj.x, obj.y,
+                        obj.width, obj.height
+                    ))
+
+                elif obj.properties['gameType'] == 'spawn':
+                    self.player_spawn = [obj.x, obj.y]
+
         self.map_group = pyscroll.PyscrollGroup(map_layer=map_layer)
 
-        self.player.position = map_layer.map_rect.center
+        self.player.position = self.player_spawn
         self.map_group.add(self.player)
 
     def update(self, dt):
-        self.map_group.update(dt, self.walls)
+        self.map_group.update(dt, self.obstacles)
+
+        if self.player.dead is True:
+            self.reset_game()
+
+    def reset_game(self):
+        self.player.position = self.player_spawn
+        self.player.dead = False
 
     def draw(self):
         x = self.player.rect.center[0]
